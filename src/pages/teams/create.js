d27a3265f14485c9084fffb6749e0670fbf3769b
DrakesWeb <d.dunenkov12.s@gmail.com>
2025-08-01 23:50:35 +0300

    Добавляю

--- src/pages/teams/create.js
+++ src/pages/teams/create.js
@@ -11,41 +11,42 @@ import Switcher from "@/components/ui/Switcher";
 import Notify from '@/assets/general/notify.svg';
 
 export default function Createteam() {
-    const [teamType, setTeamType] = useState('teacher') // student
+    const [teamType, setTeamType] = useState('student') // teacher
 
     return (
         <Layout>
             <Header>
                 <Header.Heading>Команды <span className='text-(--color-gray-black)'>/</span> Создание команды</Header.Heading>
+                <Button icon><Notify /></Button>
             </Header>
             <div className="hero">
                 <div className="col-span-4 flex flex-col">
                     <div className="flex flex-col gap-[1.25rem]">
                         <h6>Основные данные</h6>
                         <Input type="image" className="w-1/2 aspect-square"/>
-                        <Input type="text" id="nameTeam" name="nameTeam" autoComplete="nameTeam" placeholder="Название" required/>
-                        <Textarea  id="nameTeam" name="nameTeam" autoComplete="nameTeam" placeholder="Описание команды"/>
+                        <Input type="text" id="nameTeam" name="nameTeam" autoComplete="nameTeam" placeholder="Название" required />
+                        <Textarea inverted id="nameTeam" name="nameTeam" autoComplete="nameTeam" placeholder="Описание команды" />
                     </div>
                 </div>
                 <div className="col-span-4 flex flex-col">
                     <div className=" flex flex-col gap-[1.25rem]">
                         <h6>Организация и регион</h6>
                         <div className="flex flex-col gap-[.75rem]">
-                        <Textarea id="org" name="org" autoComplete="org" placeholder="Организация" required/>
-                        <Input type="text" id="region" name="region" autoComplete="region" placeholder="Регион" required/>
+                        <Textarea id="org" readOnly name="org" autoComplete="org" placeholder="Организация" />
+                        <Input type="text" readOnly id="region" name="region" autoComplete="region" placeholder="Регион" />
                         </div>
                     </div>
                 </div>
-                <div className="col-span-4 flex flex-col gap-[1.25rem]">
-                    <div>
+                <div className="col-span-4 flex flex-col h-full justify-between">
+                    <div className="flex flex-col gap-[1.25rem]">
                         <h6>Параметры</h6>
-                    </div>
-                    <div className="flex flex-col items-center h-full gap-[1.25rem]">
-                        <Switcher value={teamType} onChange={setTeamType} className="!w-full">
-                            <Switcher.Option value="teacher">преподавательская</Switcher.Option>
-                            <Switcher.Option value="student">Студентеческая</Switcher.Option>
-                        </Switcher>
-                        <p>Тип команды, для студентов или для Преподавателей</p>
+                        <div className="flex flex-col gap-[.25rem]">
+                            <Switcher value={teamType} onChange={setTeamType} className="!w-full">
+                                <Switcher.Option value="teacher" disabled>Преподавательская</Switcher.Option>
+                                <Switcher.Option value="student">Студенческая</Switcher.Option>
+                            </Switcher>
+                            <p className="small text-(--color-gray-black)">Тип команды, для студентов или для преподавателей</p>
+                        </div>
                     </div>
                     <Button disabled>Создать команду</Button>
                 </div>
