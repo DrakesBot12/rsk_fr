bbe7c0d19219006390cf00aade59a06e25448fa8
DrakesWeb <d.dunenkov12.s@gmail.com>
2025-08-01 21:41:47 +0300

    Добавляю

---
+++ src/components/pages/organizations/members.js
@@ -0,0 +1,6 @@
+export default function OrganMembersPage({ goTo }) {
+    return (
+        <>
+        </>
+    )
+}
---
+++ src/pages/organizations/[organ].js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import Layout from '@/components/layout/Layout';
+import TransitionWrapper from '@/components/layout/TransitionWrapper';
+
+
+import OrganIndexPage from '@/components/pages/organizations';
+import OrganMembersPage from '@/components/pages/organizations/members';
+
+export default function ProfilePage() {
+    const [pageKey, setPageKey] = useState("organ")
+
+    const goTo = (pageName) => {
+        setPageKey(pageName);
+    };
+
+    return (
+        <Layout>
+            <TransitionWrapper currentKey={pageKey}>
+                {pageKey === 'organ' && <OrganIndexPage goTo={goTo} />}
+                {pageKey === 'members' && <OrganMembersPage goTo={goTo} />}
+            </TransitionWrapper>
+        </Layout>
+    )
+}
