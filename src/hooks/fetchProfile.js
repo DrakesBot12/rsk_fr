4fab2fa8bbc9fa2b5d366e2d8a69b31e20649457
Mars-Lucifer <nikitarybalko897@gmail.com>
2025-10-14 10:20:49 +0300

    fix 2 projects

--- src/hooks/fetchProfile.js
+++ src/hooks/fetchProfile.js
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useRouter } from "next/navigation";
 
 export function useProfile() {
     const [data, setData] = useState("");
     const router = useRouter();
 
-    const clearCookies = () => {
+    const clearCookies = useCallback(() => {
         document.cookie.split(";").forEach((cookie) => {
             const eqPos = cookie.indexOf("=");
             const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
             document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
         });
-    };
+    }, []);
 
-    const fetchProfile = async () => {
+    const fetchProfile = useCallback(async () => {
         try {
             const response = await fetch("/api/profile/info", {
                 method: "GET",
@@ -36,7 +36,7 @@ export function useProfile() {
         } catch (err) {
             console.error("Request error:", err);
         }
-    };
+    }, [router, clearCookies]); // зависимости
 
     return { data, fetchProfile };
 }
